75a6e54b167604889de26f236ae45959d95007de
Urmas Undusk <voldemar@hotmail.com>
2024-04-09 22:34:19 +0300

    updated version info

--- hailolibero/__init__.py
+++ hailolibero/__init__.py
@@ -1,4 +1,4 @@
-from pathlib import Path
+import importlib.metadata
 
 from .hailolibero import HailoLibero
 
@@ -6,4 +6,4 @@ __all__ = [
     "HailoLibero"
 ]
 
-__version__ = (Path(__file__).parent / "VERSION").read_text().strip()
+__version__ = importlib.metadata.version("hailolibero")
